9e46e14df19d118ba7ac39a5b43c92eb3ae5a90d
Sam Huynh <samhwang2112.dev@gmail.com>
2022-12-24 01:05:09 +1100

    feat(client): install and configure react router

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,16 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { trpc } from './utils/trpc';
 import IndexPage from './index-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <IndexPage />,
+  },
+]);
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -19,7 +27,7 @@ function App() {
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <IndexPage />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </trpc.Provider>
   );
