1612b9df46416122719b2785079932e4f7abfdb2
Eric Do <beatyshot@gmail.com>
2023-01-26 23:45:54 +1000

    button teal theme

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,7 +2,7 @@ import { classed } from '@tw-classed/react'
 
 export const Button = classed(
   'button',
-  'bg-blue-500 hover:bg-blue-700',
+  'bg-teal-500 hover:bg-teal-700',
   'text-white font-bold',
   'py-2 px-4',
   'rounded'
