718d75cc9e8861df2f97cee322d3cd2eea8b6746
Sam Huynh <sam@hungryhungry.com>
2023-01-27 13:17:27 +1100

    chore: change react config loading for jotai

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,25 +2,33 @@
 
 import { defineConfig } from 'vite';
 import reactBabel from '@vitejs/plugin-react';
+import jotaiBabel from 'jotai/babel/preset';
 import reactSwc from '@vitejs/plugin-react-swc';
-import relay from "vite-plugin-relay";
+import relay from 'vite-plugin-relay';
 
 // TOGGLE THIS IF YOU SEE STRANGE BEHAVIOR IN YOUR APP.
 // See README of plugin-react-swc: https://github.com/vitejs/vite-plugin-react-swc
 const useSwc = true;
-const react = useSwc ? reactSwc : reactBabel;
+const react = (useSwc: boolean) => {
+  if (!useSwc) {
+    return reactBabel({
+      babel: {
+        presets: [jotaiBabel],
+      },
+    });
+  }
+
+  return reactSwc({
+    plugins: [
+      ['@swc-jotai/debug-label', {}],
+      ['@swc-jotai/react-refresh', {}],
+    ],
+  });
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      plugins: [
-        ["@swc-jotai/debug-label", {}],
-        ["@swc-jotai/react-refresh", {}],
-      ]
-    }),
-    relay
-  ],
+  plugins: [react(useSwc), relay],
   server: {
     proxy: {
       '/.netlify/functions': 'http://0.0.0.0:8788',
