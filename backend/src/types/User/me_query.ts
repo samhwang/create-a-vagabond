db872044cfedbea2e85f8fb9bc1545f1f35500c3
Eric Do <beatyshot@gmail.com>
2023-01-29 15:10:11 +1000

    lazily create user

--- backend/src/types/User/me_query.ts
+++ backend/src/types/User/me_query.ts
@@ -5,9 +5,37 @@ builder.queryField('me', (t) =>
   t.prismaField({
     type: 'User',
     resolve: async (_query, _root, _args, ctx) => {
-      const clerkUser = ctx.session ? await users.getUser(ctx.session?.userId) : undefined
-      console.log(clerkUser)
-      return prisma.user.findFirstOrThrow();
+      if (!ctx.session) throw new Error('Please login!')
+
+      const clerkUser = await users.getUser(ctx.session?.userId)
+      if (!clerkUser) {
+        console.log('Something went wrong!')
+        console.log(ctx.session)
+        throw new Error('Something went wrong')
+      }
+
+      const user = await prisma.user.upsert({
+        where: {
+          id: ctx.session.userId
+        },
+        update: {
+          email: clerkUser.emailAddresses[0].emailAddress,
+          username: clerkUser.username || undefined,
+          firstName: clerkUser.firstName || undefined,
+          lastName: clerkUser.lastName || undefined,
+          profileImage: clerkUser.profileImageUrl,
+        },
+        create: {
+          id: ctx.session.userId,
+          email: clerkUser.emailAddresses[0].emailAddress,
+          username: clerkUser.username || undefined,
+          firstName: clerkUser.firstName,
+          lastName: clerkUser.lastName,
+          profileImage: clerkUser.profileImageUrl,
+        },
+      })
+
+      return user
     },
   })
 );
