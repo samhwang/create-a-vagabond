edb858c765d72fe0875b4cedfcb8e7bf6b156c89
Eric Do <eric.do@go1.com>
2023-03-05 15:26:51 +1000

    fix bugs missing types import

--- workspaces/backend/src/schema.ts
+++ workspaces/backend/src/schema.ts
@@ -10,15 +10,18 @@ builder.objectType(Error, {
 builder.queryType();
 builder.mutationType();
 
+import './types/ClassMove';
+import './types/Drive';
+import './types/Equipment'
+import './types/EquipmentTag'
+import './types/Nature';
+import './types/Reputation';
+import './types/RoguishFeat';
 import './types/User';
 import './types/Vagabond';
 import './types/VagabondClass';
-import './types/Nature';
-import './types/Drive';
-import './types/Reputation';
-import './types/RoguishFeat';
-import './types/WeaponSkill';
-import './types/ClassMove';
 import './types/VagabondConnection';
+import './types/VagabondEquipment'
+import './types/WeaponSkill';
 
 export const schema = builder.toSchema();
